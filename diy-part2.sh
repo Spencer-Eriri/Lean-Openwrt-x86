a6328aa286ea16e17a3827f3f1c1e5d13123592b
Spencer-Eriri <78525581+Spencer-Eriri@users.noreply.github.com>
2024-01-18 15:43:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,15 +16,15 @@
 
 #将原版 Argon 主题删除，替换为 jerrykuku 版（18.06 分支）
 #rm -rf ~/lede/feeds/luci/themes/luci-theme-argon
-#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ~/lede/feeds/luci/themes/luci-theme-argon
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ~/lede/feeds/luci/themes/luci-theme-argon-18.06
 
 #应用默认主题为 Argon-18.06 （jerrykuku 的版本，和 lean 自带的 Argon 报名不同，不冲突，编译后在 GUI 中显示名字仍为 Argon）
 #方式是取消 Bootstrap 在 Openwrt 中的依赖地位，取而代之的是 Argon-18.06
 #但 Bootstrap 和 Argon-18.06 均会编译，在 config 中去除 Bootstrap 是无效的
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/' ~/lede/feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon-mod/' ~/lede/feeds/luci/collections/luci/Makefile
 
 
 #添加软件
-#添加jerrykuku的argon配置工具
+#添加jerrykuku的argon配置工具 Lean 已集成所以不再需要
 #git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git package/jerryk/luci-app-argon-config
 
